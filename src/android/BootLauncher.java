b11d9ef3da99781ee3623d11706b964a5c90e7fe
lagalvisg <alejo.limp@gmail.com>
2017-06-19 11:09:30 -0500

    linea13 segun
    Cordova/Phonegap seems to act differently based on the environment when generating the value you must set on the step 2. For example, if you are using Phonegap Build, it's useful if the class name is [ your app id].[your app name].class. For example, if your app id is "com.example.app" and your app name is "My Cool App", then the class name will be "com.example.app.MyCoolApp.class" (note that the spaces are removed). However, if you are using Cordova from CLI, it's supposed to use the format [your app id].MainActivity.class (on the example above, it would be com.example.app.MainActivity.class).
    
    Anyway, it's easy to get the class name by reading the AndroidManifest.xml. You will find your app id on the tag <manifest>, attribute package, and the activity name on the tag <activity>, attribute android:name.

--- src/android/BootLauncher.java
+++ src/android/BootLauncher.java
@@ -10,7 +10,7 @@ public class BootLauncher extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        Intent serviceIntent = new Intent(context, com.phonegap.helloworld.class);
+        Intent serviceIntent = new Intent(context, com.phonegap.helloworld.MainActivity.class);
         serviceIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         context.startActivity(serviceIntent);
     }
